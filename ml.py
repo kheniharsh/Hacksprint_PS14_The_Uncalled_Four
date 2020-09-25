392dc68c2f67209cbf91ddcfdd3e3000e5504dd0
kheniharsh <53462199+kheniharsh@users.noreply.github.com>
2020-09-25 21:14:39 +0530

    Add files via upload

--- ml.py
+++ ml.py
@@ -8,8 +8,6 @@ data = {
                     {
                             'a_name': "PJ Harvey",   
                             'title': "50 Ft Queenie",   
-                            'lyrics': "Hey I'm one big queen  No one can stop me Red light red green Sat back, I'm watching I'm number one  Second to no one No sweat I'm clean  Nothing can touch me  I'll tell you my name  F U C K 50ft queenie  Force ten hurricane Biggest woman  I could have ten sons Ten gods  Ten queens Ten foot and risi...",   
-                            'spot_id': "3fJprjhRxTVtOp18EHdNfu",   
                             'tempo': "126.366",   
                             'energy': "0.667",   
                             'danceability': "0.382",   
@@ -26,8 +24,8 @@ data = {
 
 body = str.encode(json.dumps(data))
 
-url = 'https://ussouthcentral.services.azureml.net/workspaces/ad9b36a5ce834694a9f8850cef897196/services/2cfe31ad93194dfd95b176ec5d1cf176/execute?api-version=2.0&format=swagger'
-api_key = 'BljlOcXnQSdpEtRf698KS4N+aPsafKTJfNIQK1zIRmg+UeenLX7ZeiX7cjFKq98izsr1Bny9vXiflAXF/Zn7vw==' # Replace this with the API key for the web service
+url = 'https://ussouthcentral.services.azureml.net/workspaces/ad9b36a5ce834694a9f8850cef897196/services/6e5a6fbeee814d898c713a9c5a1cff34/execute?api-version=2.0&format=swagger'
+api_key = 'LjQX1szCBgIHCu+2cC03YLisbTgSnX7b9O0YXj7R9IQW4URb7d/W+hLmc1MwBUfdbYxaQZHlh0Xd8PAUOI47rQ==' # Replace this with the API key for the web service
 headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
 req = urllib.request.Request(url, body, headers)
@@ -36,6 +34,7 @@ try:
     response = urllib.request.urlopen(req)
 
     result = response.read()
+    
     print(result)
 except urllib.error.HTTPError as error:
     print("The request failed with status code: " + str(error.code))
